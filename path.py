7aa25abf56cd6f118048a8fbbb7866d49a3a62c8
Lemuel <lemuelhui97@gmail.com>
2017-11-14 13:12:23 +0000

    Update path.py

--- path.py
+++ path.py
@@ -8,7 +8,7 @@ class Path:
     raw_data = os.getcwd() + '\\materials\\raw_data.csv'
     features = os.getcwd() + '\\materials\\features.pkl'
     labels = os.getcwd() + '\\materials\\labels.pkl'
-    lexicon = os.getcwd() + '\\materials\\lexicon.pkl'
+    lexicon = os.getcwd() + '\\lexicon.pkl'
     dir_dataset = os.getcwd() + '\\dataset'
     count = os.getcwd() + '\\materials\\count.pkl'
-    model = os.getcwd() + "\\materials\\model.hdf5"
+    model = os.getcwd() + "\\model.hdf5"
